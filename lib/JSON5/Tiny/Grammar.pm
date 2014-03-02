ead21a8b7ae50269e2973b6d3d0e7a2c0ca36b03
Mouq <alexmoquin@gmail.com>
2014-03-02 10:16:49 -0500

    First JSON5 commit; parse single quoted strings and idents

--- lib/JSON5/Tiny/Grammar.pm
+++ lib/JSON5/Tiny/Grammar.pm
@@ -3,10 +3,10 @@ grammar JSON::Tiny::Grammar;
 
 token TOP       { ^ \s* [ <object> | <array> ] \s* $ }
 rule object     { '{' ~ '}' <pairlist>     }
-rule pairlist   { <pair> * % \,            }
-rule pair       { <string> ':' <value>     }
+rule pairlist   { <pair> * %% \,           }
+rule pair       { <key> ':' <value>        }
 rule array      { '[' ~ ']' <arraylist>    }
-rule arraylist  {  <value> * % [ \, ]        }
+rule arraylist  {  <value> * %% [ \, ]      }
 
 proto token value {*};
 token value:sym<number> {
@@ -22,16 +22,32 @@ token value:sym<object>  { <object> };
 token value:sym<array>   { <array>  };
 token value:sym<string>  { <string> }
 
-token string {
-    \" ~ \" [ <str> | \\ <str=.str_escape> ]*
+token key { <string> | <js-ident> }
+
+token js-ident { <:alpha+[_$]> <:alpha+[_$]+[0..9]> }
+
+proto token string {*}
+token string:dbqt {
+    \" ~ \"
+    [
+    | <str>                | $<str>=\'
+    | \\ <str=.str_escape> | $<str>=[\\ \"]
+    ]*
+}
+token string:apos {
+    \' ~ \'
+    [
+    | <str>                | $<str>=\"
+    | \\ <str=.str_escape> | $<str>=[\\ \']
+    ]*
 }
 
 token str {
-    <-["\\\t\n]>+
+    <-[\\\t\n]>+
 }
 
 token str_escape {
-    <["\\/bfnrt]> | u <xdigit>**4
+    <[\\/bfnrt\n]> | u <xdigit>**4
 }
 
 # vim: ft=perl6
